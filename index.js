884d0a3e5558c2067f2671309bae13d05e2d3570
Annushka1997 <annushkach1997@mail.ru>
2022-07-13 00:11:50 +0400

    Lesson 22 with some changes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// Առաջին տարբերակ
+
 const array = [];
 const array1 = [1,2,3];
 const array2 = ["Hello","dear","Anna"];
@@ -15,8 +17,30 @@ function mySpreadFunction (item1,item2) {
     return console.log(array);
 }
 
-// mySpreadFunction(array1,array2);
 mySpreadFunction(array1,array2);
+
+// Երկրորդ տարբերակ
+
+
+const array3 = [];
+const array4 = [1,2,3];
+const array5 = ["Hello","dear","Anna"];
+
+function mySpreadFunction2 () {
+    let len = arguments.length;
+    if (len>1) {
+        for (let i=1; i<len; i++) {
+            arguments[0].push.apply(arguments[0], arguments[i]);
+        }
+    }
+    return console.log(array3);
+}
+
+mySpreadFunction2(array3,array4,array5);
+
+
+
+
 
 
 
